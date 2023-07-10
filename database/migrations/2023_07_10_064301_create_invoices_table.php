1206f1da35487538d7a51cd9237ffbb81ee71342
Muhammad Aaraif Hanif <aaraif.hanifpk1975@gmail.com>
2023-07-10 12:43:25 +0500

    API V1 OF Customers only

---
+++ database/migrations/2023_07_10_064301_create_invoices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained();
+            $table->decimal('amount', 8, 2);
+            $table->string('status'); //Billed, Paid, Cancelled
+            $table->timestamp('paid_dated')->nullable();
+            $table->timestamp('billed_dated')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
